4a18fc8a1feb56caa37d4e6306f859653731d479
adisbladis <adis@blad.is>
2016-09-27 17:47:03 +0800

    Idiomatic go fixes

--- simpleserver.go
+++ simpleserver.go
@@ -19,63 +19,63 @@ var (
 	RequestLog *log.Logger
 )
 
-func req_handler(w http.ResponseWriter, r *http.Request) {
+func reqHandler(w http.ResponseWriter, r *http.Request) {
 	cwd, _ := os.Getwd()
-	file_path := filepath.Join(cwd, r.URL.Path)
-	file_path, fp_err := filepath.EvalSymlinks(file_path)
-	if fp_err != nil {
-		log.Println(fp_err)
+	filePath := filepath.Join(cwd, r.URL.Path)
+	filePath, fpErr := filepath.EvalSymlinks(filePath)
+	if fpErr != nil {
+		log.Println(fpErr)
 		return
 	}
 
-	if strings.HasPrefix(file_path, cwd) == false {
+	if strings.HasPrefix(filePath, cwd) == false {
 		log.Println("Trying to access dir outside of cwd")
 		return
 	}
 
-	stat_info, stat_err := os.Stat(file_path)
-	if stat_err != nil {
-		log.Println(stat_err)
+	statInfo, statErr := os.Stat(filePath)
+	if statErr != nil {
+		log.Println(statErr)
 		return
 	}
 
 	RequestLog.Println(r.RemoteAddr, fmt.Sprintf("\"%s %s %s\"", r.Method, r.URL, r.Proto))
-	if stat_info.IsDir() {
-		files, read_err := ioutil.ReadDir(file_path)
-		if read_err != nil {
-			log.Println(read_err)
+	if statInfo.IsDir() {
+		files, readErr := ioutil.ReadDir(filePath)
+		if readErr != nil {
+			log.Println(readErr)
 			return
 		}
 
 		w.Header().Set("Content-Type", "text/html")
-		dirlist_title := fmt.Sprintf("Directory listing for %s", r.URL.Path)
+		dirlistTitle := fmt.Sprintf("Directory listing for %s", r.URL.Path)
 
 		fmt.Fprintf(w, "<!DOCTYPE html>\n"+
 			"<html>\n"+
 			"<head>\n"+
 			"<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\">\n"+
-			fmt.Sprintf("<title>%s</title>\n", dirlist_title)+
+			fmt.Sprintf("<title>%s</title>\n", dirlistTitle)+
 			"</head>\n<body>\n"+
-			fmt.Sprintf("<h1>%s</h1>\n<hr />\n", dirlist_title)+
+			fmt.Sprintf("<h1>%s</h1>\n<hr />\n", dirlistTitle)+
 			"<ul>\n")
 
-		list_fmt := "\t<li><a href=\"%s\">..</a></li>\n"
-		fmt.Fprintf(w, list_fmt, filepath.Dir(r.URL.Path))
+		listFmt := "\t<li><a href=\"%s\">..</a></li>\n"
+		fmt.Fprintf(w, listFmt, filepath.Dir(r.URL.Path))
 		for _, f := range files {
-			f_full := html.EscapeString(filepath.Join(r.URL.Path, f.Name()))
-			fmt.Fprintf(w, "\t<li><a href=\"%s\">%s</a></li>\n", f_full, f.Name())
+			fFull := html.EscapeString(filepath.Join(r.URL.Path, f.Name()))
+			fmt.Fprintf(w, "\t<li><a href=\"%s\">%s</a></li>\n", fFull, f.Name())
 		}
 
 		fmt.Fprintf(w, "</ul>\n<hr />\n</body>\n</html>")
 
 	} else {
-		mime_type := mime.TypeByExtension(filepath.Ext(file_path))
-		if mime_type == "" {
-			mime_type = "application/octet-stream"
+		mimeType := mime.TypeByExtension(filepath.Ext(filePath))
+		if mimeType == "" {
+			mimeType = "application/octet-stream"
 		}
-		w.Header().Set("Content-Type", mime_type)
+		w.Header().Set("Content-Type", mimeType)
 
-		file, err := os.Open(file_path)
+		file, err := os.Open(filePath)
 		if err != nil {
 			log.Println(err)
 			return
@@ -105,6 +105,6 @@ func main() {
 		"REQ: ",
 		log.Ldate|log.Ltime)
 
-	http.HandleFunc("/", req_handler)
+	http.HandleFunc("/", reqHandler)
 	http.ListenAndServe(":8000", nil)
 }
